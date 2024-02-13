91e3236ecedadad14b0edad058ccd7b5acaaea33
VitaliiChichuga <chichygav@gmail.com>
2024-02-13 17:41:25 +0200

    add v7

--- js/index.js
+++ js/index.js
@@ -6,3 +6,41 @@ const newStringDate =
   dateString[1] + ' ' + dateString[2] + ', ' + dateString[3];
 
 categoriesDate.innerText = newStringDate;
+
+// ===========
+console.log(window.innerWidth);
+let event = null;
+
+document.addEventListener('touchstart', function (e) {
+  event = e;
+});
+document.addEventListener('touchstart', function (e) {
+  if (event) {
+    console.log(e.touches[0].pageX);
+  }
+});
+document.addEventListener('touchend', function (e) {
+  if (event) {
+    console.log(event.touches[0].pageX);
+  }
+});
+// document.addEventListener('touchmove', function (e) {
+//   if (event) {
+//     console.log('Move delta: ' + (e.touches[0].pageX - event.touches[0].pageX));
+//   }
+// });
+document.addEventListener('touched', function (e) {
+  event = null;
+});
+// const firstItem = document.querySelector('.mazes__item--main');
+// document.addEventListener('touchmove', () => {
+//   firstItem.style.marginLeft = -100 + '%';
+// });
+// document.addEventListener('touchstart', () => {
+//   console.log(firstItem.pageYOffset);
+//   console.log(window.pageXOffset);
+// });
+// document.addEventListener('touchend', () => {
+//   console.log(firstItem.pageYOffset);
+//   console.log(window.pageXOffset);
+// });
