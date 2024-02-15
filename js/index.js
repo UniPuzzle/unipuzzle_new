02eff7be400f9c3dd50aa1191caed3e05cbd80e7
VitaliiChichuga <chichygav@gmail.com>
2024-02-15 18:18:55 +0200

    add v11 - mobile modal nav

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ const newStringDate =
   dateString[1] + ' ' + dateString[2] + ', ' + dateString[3];
 
 categoriesDate.innerText = newStringDate;
+
+const headerMenu = document.querySelector('.header-menu');
+const body = document.querySelector('.body');
+
+window.addEventListener('click', e => {
+  if (e.target.classList.contains('close__btn')) {
+    headerMenu.classList.toggle('show');
+    body.style.overflow = 'auto';
+  }
+  if (e.target.classList.contains('burger__btn')) {
+    headerMenu.classList.toggle('show');
+    body.style.overflow = 'hidden';
+  }
+});
