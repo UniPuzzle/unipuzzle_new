a304c31b1763b730e26cb9badbc3f7119e078590
VitaliiChichuga <chichygav@gmail.com>
2024-03-19 16:06:26 +0200

    add header

--- js/up_header.js
+++ js/up_header.js
@@ -1,4 +1,3 @@
-const categoriesDate = document.querySelector('.categories__date');
 const seasonText = document.querySelector('.header .letter__text');
 const headerIntroLetter = document.querySelector(
   '.header .intro__title-letter',
@@ -6,13 +5,7 @@ const headerIntroLetter = document.querySelector(
 
 const date = new Date();
 
-const dateString = date.toDateString().split(' ');
-const newStringDate =
-  dateString[1] + ' ' + dateString[2] + ', ' + dateString[3];
-
-categoriesDate.innerText = newStringDate;
-
-// ==========
+// =====seasons and first letter=====
 
 const month = date.getMonth();
 
@@ -49,12 +42,9 @@ switch (month) {
   default:
     console.log('Invalid month');
 }
-
-// ============
-
 headerIntroLetter.style.width = headerIntroLetter.clientHeight + 'px';
 
-// =====================
+// ==========burger-menu===========
 const headerMenu = document.querySelector('.header-menu');
 const body = document.querySelector('.body');
 const burgerBtn = document.querySelector('.burger__btn');
@@ -62,18 +52,4 @@ const burgerBtn = document.querySelector('.burger__btn');
 burgerBtn.addEventListener('click', () => {
   headerMenu.classList.toggle('show');
   burgerBtn.classList.toggle('close');
-
-  // if (e.target.classList.contains('burger__btn')) {
-  //   headerMenu.classList.toggle('show');
-  // }
 });
-// window.addEventListener('click', e => {
-//   if (e.target.classList.contains('close__btn')) {
-//     headerMenu.classList.toggle('show');
-//     body.style.overflow = 'auto';
-//   }
-//   if (e.target.classList.contains('burger__btn')) {
-//     headerMenu.classList.toggle('show');
-//     body.style.overflow = 'hidden';
-//   }
-// });
